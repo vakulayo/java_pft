54f94427a1454825d9a8feb77288c97482471c09
vakulayo <vakula.yo@gmail.com>
2016-04-25 22:53:22 +0300

    add several functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,32 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-	System.out.println("Hello, world!");
+	  hello("world");
+    hello("user");
+    hello("Kate");
+
+    double l=7.0;
+    System.out.println("Площадь квадрата со стороной "+l+" равна "+area(l));
+
+    double a=4;
+    double b=6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a,b));
+
 	}
+
+  public static void hello(String somebody){
+
+    System.out.println("Hello,"+somebody+"!");
+
+  }
+
+
+  public static double area(double len){
+    return len*len;
+  }
+
+  public static double area (double a, double b){
+    return a*b;
+  }
 
 }
